5b9efe19cf732674915e2289752b9ff6a94be947
siguma <siguma@localhost.localdomain>
2021-08-22 05:48:25 +0900

    テンプレートからプロジェクト情報を修正

---
+++ src/main/kotlin/siguma0013/container_storage/ContainerStorage.kt
@@ -0,0 +1,9 @@
+package siguma0013.container_storage
+import net.fabricmc.api.ModInitializer
+
+object ContainerStorage : ModInitializer {
+    private const val MOD_ID = "container_storage"
+    override fun onInitialize() {
+        println(MOD_ID)
+    }
+}
